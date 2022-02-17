afc77baecc5572a4d8083f5bb80b471dca55cd20
shoronshaha <smaranshaha@gmail.com>
2022-02-17 18:30:58 +0600

    complete

--- js/app.js
+++ js/app.js
@@ -1,31 +1,36 @@
 //functions with error handling
+
 let flag = false;
-function getValue(inputId, err1, err2) {
+function getValue(inputId, error1, error2) {
     let input = document.getElementById(inputId);
     let number = parseFloat(input.value);
-    let errMsg1 = document.getElementById(err1);
-    let errMsg2 = document.getElementById(err2);
+    let errorMsg1 = document.getElementById(error1);
+    let errorMsg2 = document.getElementById(error2);
+
     if (Number.isInteger(number) == false) {
-        errMsg1.style.display = 'block';
-        errMsg2.style.display = 'none';
+        errorMsg1.style.display = 'block';
+        errorMsg2.style.display = 'none';
         flag = true;
         return 'invalid input...!!, please give an integer number';
     }
     else if (number < 0) {
-        errMsg1.style.display = 'none';
-        errMsg2.style.display = 'block';
+        errorMsg1.style.display = 'none';
+        errorMsg2.style.display = 'block';
         flag = true;
         return 'invalid input...!!, please give a positive number';
     }
-    errMsg1.style.display = 'none';
-    errMsg2.style.display = 'none';
+    errorMsg1.style.display = 'none';
+    errorMsg2.style.display = 'none';
     return number;
 }
+
 function setValue(num, setId) {
     let setfield = document.getElementById(setId);
     setfield.innerText = num;
 }
+
 //calculation of balance and expense
+
 function calculate() {
     flag = false;
     let income = getValue('income', 'incomeErr1', 'incomeErr2');
@@ -48,6 +53,7 @@ function calculate() {
     setValue(expense, 'expense');
     setValue(balance, 'balance');
 }
+
 //savings and remaining balance calculation
 function savingsCalc() {
     flag = false;
